a4815a7f4df148aa424afefb7122f2c462f3a1f3
xuguangde <784894528@qq.com>
2020-04-13 18:57:26 +0800

    最新版

---
+++ component/notice/notice.js
@@ -0,0 +1,63 @@
+Component({
+  /**
+   * 组件的属性列表
+   */
+  properties: {
+    
+  },
+
+  /**
+   * 组件的初始数据
+   */
+  data: {
+    contentList: ['明明于9:23，成功查看了3次！', '红红于9:23，成功查看了5次！', '兰兰于9:23，成功查看了8次！'],
+    listI: -1,
+    content: '欢迎进入小程序'
+  },
+  attached: function () {
+    setInterval(() => { this.update(this.getListInfo()) }, 3000)
+  },
+  /**
+   * 组件的方法列表
+   */
+  methods: {
+    update(content) {
+      var that = this
+      var animation = wx.createAnimation()
+          // 旧消息向上平移
+      animation.translateY(-30).step({
+              duration: 1000,
+              timingFunction: 'ease-in'
+          })
+          // 为了实现下一条新内容向上平移的效果，必须把内容很快平移到下方，并且不能被用户看见，这里其原理类似轮播图的思路。
+          // 实现方法：动画时间设置为1ms，过渡效果设置为’动画第一帧就跳至结束状态直到结束‘
+      animation.opacity(0).translateY(30).step({
+              duration: 1,
+              timingFunction: 'step-start'
+          })
+          // 新消息向上平移的同时恢复透明
+      animation.opacity(1).translateY(0).step({
+          duration: 1000,
+          timingFunction: 'ease-out'
+      })
+      that.setData({
+              animationData: animation.export()
+          })
+          // 更新内容的延时必须大于第一步动画时间
+      setTimeout(() => {
+          that.setData({
+              content: content
+          })
+      }, 1000)
+    },
+    getListInfo() {
+        if (this.data.listI >= this.data.contentList.length - 1) {
+            this.data.listI = -1
+            this.getListInfo()
+        } else {
+            this.data.listI++
+        }
+        return this.data.contentList[this.data.listI]
+    }
+  }
+})
--- pages/index/index.js
+++ pages/index/index.js
@@ -7,7 +7,14 @@ Page({
     motto: 'Hello World',
     userInfo: {},
     hasUserInfo: false,
-    canIUse: wx.canIUse('button.open-type.getUserInfo')
+    canIUse: wx.canIUse('button.open-type.getUserInfo'),
+    swiperImgUrls:[
+      {img:'https://ss3.bdstatic.com/70cFv8Sh_Q1YnxGkpoWK1HF6hhy/it/u=3839775455,858941348&fm=26&gp=0.jpg'},
+      {img:'https://ss3.bdstatic.com/70cFv8Sh_Q1YnxGkpoWK1HF6hhy/it/u=3839775455,858941348&fm=26&gp=0.jpg'},
+      {img:'https://ss3.bdstatic.com/70cFv8Sh_Q1YnxGkpoWK1HF6hhy/it/u=3839775455,858941348&fm=26&gp=0.jpg'},
+      {img:'https://ss3.bdstatic.com/70cFv8Sh_Q1YnxGkpoWK1HF6hhy/it/u=3839775455,858941348&fm=26&gp=0.jpg'}
+    ],
+    swiperCurrent: 0,
   },
   //事件处理函数
   bindViewTap: function() {
@@ -15,36 +22,19 @@ Page({
       url: '../logs/logs'
     })
   },
+  swiperChange(e) {
+    const that = this;
+    that.setData({
+      swiperCurrent: e.detail.current
+    })
+  },
   onLoad: function () {
-    if (app.globalData.userInfo) {
-      this.setData({
-        userInfo: app.globalData.userInfo,
-        hasUserInfo: true
-      })
-    } else if (this.data.canIUse){
-      // 由于 getUserInfo 是网络请求，可能会在 Page.onLoad 之后才返回
-      // 所以此处加入 callback 以防止这种情况
-      app.userInfoReadyCallback = res => {
-        this.setData({
-          userInfo: res.userInfo,
-          hasUserInfo: true
-        })
-      }
-    } else {
-      // 在没有 open-type=getUserInfo 版本的兼容处理
-      wx.getUserInfo({
-        success: res => {
-          app.globalData.userInfo = res.userInfo
-          this.setData({
-            userInfo: res.userInfo,
-            hasUserInfo: true
-          })
-        }
-      })
-    }
+    console.log(this.data.swiperImgUrls[0].img)
+  },
+  search(e){
+    console.log(e.detail)
   },
   getUserInfo: function(e) {
-    console.log(e)
     app.globalData.userInfo = e.detail.userInfo
     this.setData({
       userInfo: e.detail.userInfo,
